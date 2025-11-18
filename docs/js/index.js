da2862ba9d3349fc925d46da288c6d5f4e8e9123
Erikos Mac <alerik9+github@gmail.com>
2025-11-18 12:44:15 +0100

    Working setting and getting of project id, key and name

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,54 +1,119 @@
-let db_key = "";
-let project_id = "";
+const div_projectData = document.getElementById("get_project_data");
+const div_getName = document.getElementById("get_name");
+let request_data;
+let supabaseClient;
 
-function saveCookie() {
-    const key = document.getElementById("DBKey").value;
-    const id = document.getElementById("DBId").value;
+async function saveDBCookies() {
+    // Get input values
+    const project_key = document.getElementById("db_key").value;
+    const project_id = document.getElementById("db_id").value;
 
-    if (!key) {
-        return alert("Please enter a for the DB key!");
+    // Check if values are empty
+    if (!project_key) {
+        return alert("Bitte gib einen DB Schlüssel ein!");
     }
-    if (!id) {
-        return alert("Please enter a for the DB id!");
+    if (!project_id) {
+        return alert("Bitte gib eine DB ID ein!");
     }
 
-    // Set expiration 
-    // date: yyyy-mm-ddThh:mm:ssZ
-    const expireDate = new Date("2026-10-10T00:00:00Z").toUTCString();
-    document.cookie = `DBKey=${encodeURIComponent(key)}; expires=${expireDate}; path=/`;
-    document.cookie = `DBId=${encodeURIComponent(id)}; expires=${expireDate}; path=/`;
+    // Set expiration format: yyyy-mm-ddThh:mm:ssZ
+    const expireDate = new Date("2028-12-12T00:00:00Z").toUTCString(); //TODO: make date relative
+   
+    // Set cookies
+    document.cookie = `db_key=${encodeURIComponent(project_key)}; expires=${expireDate}; path=/`;
+    document.cookie = `db_id=${encodeURIComponent(project_id)}; expires=${expireDate}; path=/`;
 
-    alert("Saved!")
+    // Get label to write available names in
+    const namesLabel = document.getElementById("names");
+
+    // Get all names
+    data = await fetchPeople(project_id, project_key);
+    request_data = data.data;
+    const request_error = data.error;
+    if (request_error) {
+        console.log(request_error);
+        return alert("Die Daten waren leider nicht korrekt:\n" + JSON.stringify(request_error));
+    }
+
+    // filter out the names from request
+    names = [];
+    for (let i = 0; i < request_data.length; ++i) {
+        names[i] = ' ' + request_data[i].name ;
+    }
+    
+    // Set content of label to names list
+    namesLabel.textContent = names;
+
+    div_projectData.style.display = "none";
+    div_getName.style.display = "block";
+}
+
+async function saveNameCookie() {
+    // Get input values
+    const name_input = document.getElementById("name").value;
+    const in_list = document.querySelector('input[name="name_in_list"]:checked')?.value;
+
+    // Check if values are empty
+    if (!name_input) {
+        return alert("Bitte gib einen Namen ein!");
+    }
+    if (!in_list) {
+        return alert("Ist der Namen aus der Liste gewesen?");
+    }
+
+    if (in_list === "new_item") {
+        const {data, error} = await supabaseClient.from('People').insert([{name: name_input}]);
+        
+        if (error) { // TODO: if element is in db but new item selected show warning of duplicate
+            console.log(error);
+            return alert("Etwas ist schief gelaufen (siehe console.log() für mehr Details).");
+        }
+    } else if (in_list === "in_list") {
+        const {data, error} = await supabaseClient
+                                        .from('People')
+                                        .select('*')
+                                        .eq('name', name_input);
+        if (error) {
+            console.log(data);
+            return alert("Etwas ist schief gelaufen (siehe console.log() für mehr Details).");
+        }
+        if (data.length === 0) {
+            return alert("Name konnte nicht gefunden werden.");
+        }
+    }
+
+    // Set expiration format: yyyy-mm-ddThh:mm:ssZ
+    const expireDate = new Date("2028-12-12T00:00:00Z").toUTCString(); //TODO: make date relative
+   
+    // Set cookies
+    document.cookie = `db_username=${encodeURIComponent(name_input)}; expires=${expireDate}; path=/`;
+
+    document.body.insertAdjacentHTML("beforeend", "<h3>Name (" + name_input + ") erfolgreich gespeichert!<h3>")
 }
 
 
-function getCookie() {
+function getCookie(cookie_name) {
     const cookies = document.cookie.split('; ');
-    const dbKeyCookie = cookies.find(row => row.startsWith('DBKey='));
-    const dbIdCookie = cookies.find(row => row.startsWith('DBId='));
+    const varCookie = cookies.find(row => row.startsWith(cookie_name + '='));
+    let value = "";
 
-    if (dbKeyCookie && dbIdCookie) {
-        db_key = decodeURIComponent(dbKeyCookie.split('=')[1]);
-        project_id = decodeURIComponent(dbIdCookie.split('=')[1]);
-        console.log("DBKey: " + dbKeyCookie);
-        console.log("DBKey: " + dbIdCookie);
+    if (varCookie) {
+        value = decodeURIComponent(varCookie.split('=')[1]);
+        console.log("cookie: " + varCookie);
     } else {
+        value = undefined;
         console.log("DBKey or DBId cookie not found.");
     }   
-
+    return value;
 }
 
-async function fetchPeople() {
-    console.log(db_key);
-    console.log(project_id);
-
+async function fetchPeople(project_id, db_key) {
     // Initialize Supabase
     const SUPABASE_URL = 'https://' + project_id + '.supabase.co';
     const SUPABASE_ANON_KEY = db_key;
 
-    const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-    const { data, error } = await supabaseClient.from('Time').select('*');
-    console.log(data, error);
-    
+    const { data, error } = await supabaseClient.from('People').select('*');
+    return { data, error };
 }
